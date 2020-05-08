94907ca37a3523dd135b4535da685da3c7462522
yideliang <deliang001@e.ntu.edu.sg>
2020-05-08 15:59:36 +0800

    update

--- main.go
+++ main.go
@@ -4,6 +4,42 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
-	var a string = "abc"
+	var a = "abc"
 	fmt.Println(a)
+
+	_, numb, strs := number()
+	fmt.Println(numb, strs)
+
+	primenumber()
+	//fmt.Println("\n")
+
+	res := fibonacci(10)
+	fmt.Println(res)
+}
+
+func number()(int,int,string){
+	var a = 1
+	b, c := 2, "dennis"
+	return a, b, c
+}
+
+func primenumber(){
+	var i, j int
+	for i=2; i<100; i++{
+		for j=2; j<=(i/j); j++{
+			if i%j==0 {
+				break
+			}
+		}
+		if j>(i/j) {
+			fmt.Printf("%d ", i)
+		}
+	}
+}
+
+func fibonacci(n int) int{
+	if n < 2{
+		return n
+	}
+	return fibonacci(n-1) + fibonacci(n-2)
 }
