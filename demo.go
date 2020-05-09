c9abd3bf8a975a6c1e0a0cf5657714eb571f751f
yideliang <deliang001@e.ntu.edu.sg>
2020-05-09 17:25:38 +0800

    17:25update

--- demo.go
+++ demo.go
@@ -1,20 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
-func main() {
-	fmt.Println("Hello, World!")
-	var a = "abc"
-	fmt.Println(a)
+type Student struct {
+	Name string
+	ID int
+	Course []string
+}
 
-	_, numb, strs := number()
-	fmt.Println(numb, strs)
-
-	primenumber()
-	//fmt.Println("\n")
-
-	res := fibonacci(10)
-	fmt.Println(res)
+func structtojson(){
+	group := Student{
+		Name: "Dennis",
+		ID: 19941024 ,
+		Course: []string{"math", "science", "chinese"},
+	}
+	b, err := json.Marshal(group)
+	if err != nil{
+		fmt.Println("json.marshal failed, err:", err)
+		return
+	}
+	fmt.Printf("%s\n", string(b))
 }
 
 func number()(int,int,string){
@@ -43,4 +51,23 @@ func fibonacci(n int) int{
 	}
 	return fibonacci(n-1) + fibonacci(n-2)
 }
+
+func main() {
+	fmt.Println("Hello, World!")
+	var a = "abc"
+	fmt.Println(a)
+
+	_, numb, strs := number()
+	fmt.Println(numb, strs)
+
+	primenumber()
+	//fmt.Println("\n")
+
+	res := fibonacci(10)
+	fmt.Println(res)
+
+	structtojson()
+}
+
+
 
