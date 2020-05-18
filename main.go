fca74923ee98e15c21f97d33c0dcd668783f8c41
yideliang <deliang001@e.ntu.edu.sg>
2020-05-18 15:20:17 +0800

    20200518

--- main.go
+++ main.go
@@ -27,4 +27,9 @@ func main() {
 	var nums = []int{-2,1,-3,4,-1,2,1,-5,4}
 	res1 := maxSubArray(nums)
 	fmt.Println(res1)
+
+	var nums1 = []int{3,2,3,1,2,4,5,5,6}
+	k := 4
+	res2 := findKthLargest(nums1, k-1)
+	fmt.Println(res2)
 }
